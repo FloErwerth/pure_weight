178a1443bb3e81cf83466e871dfdb520d7a9855f
Florian Erwerth <erwerthflorian@outlook.de>
2023-10-20 09:28:23 +0200

    add clear button to plain input

--- components/PlainInput/styles.ts
+++ components/PlainInput/styles.ts
@@ -2,10 +2,21 @@ import { StyleSheet } from "react-native";
 import { mainColor } from "../App/theme/colors";
 
 export const styles = StyleSheet.create({
-  innerWrapper: {
+  wrapper: {
     alignSelf: "stretch",
     padding: 5,
     color: mainColor,
+  },
+  clearWrapper: {
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  innerWrapper: {
+    alignSelf: "stretch",
+    justifyContent: "space-around",
+    marginHorizontal: 10,
+    alignItems: "center",
+    gap: 10,
   },
   inputStyle: {
     borderWidth: 0,
