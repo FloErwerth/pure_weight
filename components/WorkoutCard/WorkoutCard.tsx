80cc7cd7f58877e9dc28e84f8909a637026adb21
Florian Erwerth <erwerthflorian@outlook.de>
2023-11-06 06:46:06 +0100

    use only one container, export background color from hook

--- components/WorkoutCard/WorkoutCard.tsx
+++ components/WorkoutCard/WorkoutCard.tsx
@@ -25,21 +25,39 @@ const EDIT_THRESHOLD = 60;
 const useWorkoutGesturePan = ({ onEdit, onDelete }: { onEdit: () => void; onDelete: () => void }) => {
   const gesture = useRef(Gesture.Pan()).current;
   const offsetX = useRef(new Animated.Value(0)).current;
+  const [active, setActive] = useState(false);
+  const { secondaryBackgroundColor } = useTheme();
+  const [isDelete, setIsDelete] = useState<boolean | undefined>(false);
+  const handleGestureStart = useCallback(() => {
+    setActive(true);
+  }, []);
 
+  const interpolatedBackgroundColor = useMemo(() => {
+    if (isDelete) {
+      return offsetX.interpolate({
+        inputRange: [-60, -40, 0],
+        outputRange: ["red", secondaryBackgroundColor, secondaryBackgroundColor],
+      });
+    } else {
+      return offsetX.interpolate({
+        inputRange: [0, 20, 40],
+        outputRange: [secondaryBackgroundColor, "#114433", "#116633"],
+      });
+    }
+  }, []);
   const handleGestureUpdate = useCallback(
     (e: GestureUpdateEvent<PanGestureHandlerEventPayload & PanGestureChangeEventPayload>) => {
       const translation = e.translationX;
-      if (translation > 0) {
-        if (translation < EDIT_THRESHOLD) {
-          offsetX.setValue(translation);
-        }
-      } else {
-        if (translation > DELETE_THRESHOLD) {
-          offsetX.setValue(translation);
+      offsetX.setValue(translation);
+      if (isDelete === undefined) {
+        if (translation > 0) {
+          setIsDelete(true);
+        } else {
+          setIsDelete(false);
         }
       }
     },
-    [offsetX],
+    [isDelete, offsetX],
   );
 
   const handleGestureEnd = useCallback(
@@ -56,6 +74,8 @@ const useWorkoutGesturePan = ({ onEdit, onDelete }: { onEdit: () => void; onDele
           void Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Medium);
         }
       }
+      setIsDelete(undefined);
+      setActive(false);
       Animated.timing(offsetX, {
         toValue: 0,
         duration: 100,
@@ -72,68 +92,53 @@ const useWorkoutGesturePan = ({ onEdit, onDelete }: { onEdit: () => void; onDele
     userSelect: "none",
     needsPointerData: true,
   };
+
+  gesture.onBegin(handleGestureStart);
   gesture.onChange(handleGestureUpdate);
   gesture.onEnd(handleGestureEnd);
 
-  return [gesture, offsetX] as const;
+  return [gesture, offsetX, active, interpolatedBackgroundColor] as const;
 };
 
 export const WorkoutCard = ({ handleNavigateToProgress, overallTrainingData, workoutName, onEdit, onDelete, onClick }: WorkoutCardProps) => {
-  const [gesture, offsetX] = useWorkoutGesturePan({ onEdit, onDelete });
+  const [gesture, offsetX, active, interpolatedBackgroundColor] = useWorkoutGesturePan({ onEdit, onDelete });
   const { secondaryBackgroundColor, mainColor } = useTheme();
   const viewRef = useRef<View>(null);
-  const [containerHeight, setContainerHeight] = useState(120);
+  const [containerMeasures, setContainerMeasures] = useState<{ width: number; height: number }>({ width: 200, height: 120 });
   const containerMeasurement = useCallback(() => {
     viewRef.current?.measure((x, y, width, height) => {
-      setContainerHeight(height - 10);
+      setContainerMeasures({ width, height: height - 10 });
     });
   }, []);
 
-  const wrapperStyles = useMemo(() => [styles.animatedWrapper, { transform: [{ translateX: offsetX }] }], [offsetX]);
-  const editContainerStyles = useMemo(
-    () => [
-      styles.iconContainer,
-      {
-        left: 0,
-        height: containerHeight,
-        backgroundColor: offsetX.interpolate({
-          inputRange: [0, 20, 60],
-          outputRange: [secondaryBackgroundColor, secondaryBackgroundColor, "#116633"],
-        }),
-      },
-    ],
-    [containerHeight, offsetX, secondaryBackgroundColor],
-  );
+  const animatedWrapperStyles = useMemo(() => [styles.animatedWrapper, { transform: [{ translateX: offsetX }] }], [offsetX]);
+  const wrapperStyles = useMemo(() => [styles.wrapper, { pointerEvents: active ? "none" : "auto" } as const], [active]);
 
-  const deleteContainerStyles = useMemo(
+  const iconContainerStyles = useMemo(
     () => [
       styles.iconContainer,
       {
-        height: containerHeight,
+        height: containerMeasures.height,
         right: 0,
-        backgroundColor: offsetX.interpolate({
-          inputRange: [-60, -20, 0],
-          outputRange: ["red", secondaryBackgroundColor, secondaryBackgroundColor],
-        }),
+        width: containerMeasures.width,
+        backgroundColor: interpolatedBackgroundColor,
       },
     ],
-    [containerHeight, offsetX, secondaryBackgroundColor],
+    [containerMeasures, interpolatedBackgroundColor],
   );
 
   return (
     <GestureDetector gesture={gesture}>
       <Pressable onPress={onClick}>
         <View ref={viewRef} onLayout={containerMeasurement}>
-          <Animated.View style={wrapperStyles}>
-            <ThemedView component style={styles.wrapper}>
+          <Animated.View style={animatedWrapperStyles}>
+            <ThemedView component style={wrapperStyles}>
               <Text style={styles.title}>{workoutName}</Text>
               <ProgressDisplay handleNavigateToProgress={handleNavigateToProgress} overallTrainingData={overallTrainingData} />
             </ThemedView>
           </Animated.View>
-          <Animated.View style={editContainerStyles}>
+          <Animated.View style={iconContainerStyles}>
             <MaterialCommunityIcons style={styles.editIcon} color={mainColor} name="pencil" size={30} />
-          </Animated.View>
-          <Animated.View style={deleteContainerStyles}>
             <MaterialCommunityIcons style={styles.deleteIcon} color={mainColor} name="delete" size={32} />
           </Animated.View>
         </View>
