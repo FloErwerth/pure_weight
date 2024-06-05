f8a4ad80a95a3328fc5658006c890c6d6b779e3b
Florian Erwerth <erwerthflorian@outlook.de>
2024-06-05 08:44:38 +0200

    remove unused code

--- utils/date.ts
+++ utils/date.ts
@@ -8,19 +8,6 @@ export const getDateTodayIso = (): IsoDate => {
     return isoDateDecoder.parse(now);
 };
 
-export function getMonthDiff(d1: Date, d2: Date) {
-    let months;
-    months = (d2.getFullYear() - d1.getFullYear()) * 12;
-    months -= d1.getMonth();
-    months += d2.getMonth();
-    return months <= 0 ? 0 : months;
-}
-
-export const getIsoDate = (timestamp: number | string) => {
-    const convertedTimestamp = typeof timestamp === "string" ? parseInt(timestamp) : timestamp;
-    return Temporal.Instant.fromEpochMilliseconds(convertedTimestamp).toString().split("T")[0] as IsoDate;
-};
-
 export const getLocaleDate = (date?: IsoDate, language?: "en" | "de", options?: Intl.DateTimeFormatOptions) => {
     if (!date) {
         return undefined;
@@ -29,12 +16,6 @@ export const getLocaleDate = (date?: IsoDate, language?: "en" | "de", options?: 
 };
 
 export const convertDate = {
-    toIsoDate: (date?: Date) => {
-        if (!date) {
-            return getDateTodayIso();
-        }
-        return Temporal.Instant.from(date.toISOString()).toString().split("T")[0] as IsoDate;
-    },
     toDate: (isoDate: IsoDate) => {
         return new Date(isoDate);
     },
@@ -43,18 +24,6 @@ export const convertDate = {
     },
 };
 
-export const getMonth = (date?: IsoDate) => {
-    if (!date) {
-        return -1;
-    } else {
-        return Temporal.PlainDate.from(date).month;
-    }
-};
-
 export const getMonthYearLabel = (date: IsoDate) => {
     return Temporal.PlainDate.from(date).toLocaleString(Locale.locale, { month: "long", year: "numeric" });
-};
-
-export const getEpochMilliseconds = (date: IsoDate) => {
-    return Temporal.Instant.from(`${date}T00:00+00:00`).epochMilliseconds;
 };
